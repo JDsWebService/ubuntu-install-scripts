b6c7604404aecfd3c1c2c332f7746ae472b2389f
Jonathan Robinson <jdswebservice@gmail.com>
2020-10-02 22:25:24 -0400

    Added Sublime Text 3 Install Script

---
+++ install-sublime3.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+sudo apt update &&
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common &&
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/" &&
+sudo apt update &&
+sudo apt -y upgrade &&
+sudo apt -y install sublime-text
